ba81b9e2e070faee315068e56b7a96a3b7a0c805
Rianov Viacheslav <slavarianov@yandex.ru>
2022-05-31 16:57:01 +0300

    new collection activity upd

---
+++ app/src/main/java/a06367/apicture/ext/BitmapExt.kt
@@ -0,0 +1,60 @@
+package a06367.apicture.ext
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+import a06367.apicture.APictureApp
+import a06367.apicture.utils.Constants
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.URL
+
+// extension function to get / download bitmap from url
+fun URL.toBitmap(): Bitmap? {
+    return try {
+        BitmapFactory.decodeStream(openStream())
+    } catch (e: IOException) {
+        Log.d(Constants.TAG, e.toString())
+        return null
+    }
+}
+
+// extension function to save an image to internal storage
+fun Bitmap.saveToInternalStorage(name: String): Uri? {
+    val context = APictureApp.applicationContext()
+    // initializing a new file
+    // bellow line return a directory in internal storage
+    val fileName = "${name}.jpg"
+    val file = File(
+        context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+            .toString() + File.separator + fileName
+    )
+    if (file.exists()) {
+        return Uri.parse(file.absolutePath)
+    } else {
+        return try {
+            // get the file output stream
+            val stream: OutputStream = FileOutputStream(file)
+
+            // compress bitmap
+            compress(Bitmap.CompressFormat.JPEG, Constants.IMAGE_QUALITY, stream)
+
+            // flush the stream
+            stream.flush()
+
+            // close stream
+            stream.close()
+
+            // return the saved image uri
+            Uri.parse(file.absolutePath)
+        } catch (e: IOException) { // catch the exception
+            e.printStackTrace()
+            Log.d(Constants.TAG, e.toString())
+            null
+        }
+    }
+}
